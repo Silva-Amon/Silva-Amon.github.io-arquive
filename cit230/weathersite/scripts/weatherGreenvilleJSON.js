5760c71788c75ddf135a39171dedca242cfa042d
Silva-Amon <amon.bernardo@hotmail.com>
2018-03-21 15:13:02 -0300

    showing the weather

--- cit230/weathersite/scripts/weatherGreenvilleJSON.js
+++ cit230/weathersite/scripts/weatherGreenvilleJSON.js
@@ -1,6 +1,6 @@
 var weatherObject = new XMLHttpRequest();
 
-weatherObject.open('GET', 'http://api.wunderground.com/api/3a922c17c0b56de7/conditions/q/MN/Franklin.json', true);
+weatherObject.open('GET', 'https://api.wunderground.com/api/3a922c17c0b56de7/conditions/q/MN/Franklin.json', true);
 
 weatherObject.send();
 
