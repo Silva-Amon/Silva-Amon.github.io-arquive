7702c834ba12ef32ae9420c40d875437917fe6ef
Silva-Amon <amon.bernardo@hotmail.com>
2018-03-26 17:57:40 -0300

    WindChild added to the getJson to make a precise calculus about the WindChild

--- cit230/weathersite/scripts/weatherFranklinJSON.js
+++ cit230/weathersite/scripts/weatherFranklinJSON.js
@@ -14,5 +14,17 @@ weatherObject.onload = function() {
     document.getElementById('temp').innerHTML = weatherInfo.current_observation.temp_f;
     document.getElementById('w_icon').src = weatherInfo.current_observation.icon_url;
     document.getElementById('prec').innerHTML = weatherInfo.current_observation.precip_1hr_in*100;
-    
+
+    //calculate the windchill.
+    var high = parseFloat(document.getElementById("high").textContent); //getting high temp.
+    var low = parseFloat(document.getElementById("low").textContent); //getting low temp.
+    var windSpeed = parseFloat(document.getElementById("wind").textContent); //getting the wind speed per hour.
+
+    var t = (high + low) / 2; //wind speed
+
+    var f = 35.74 + 0.6215 * t - 35.75 *  Math.pow(windSpeed, 0.16) + 0.4275 * t * Math.pow(windSpeed, 0.16); //Computing the wind chill factor.
+
+    var windFactor = Math.round(f*100)/100; //round to decimal places.
+
+    document.getElementById("windChillF").innerHTML = " " + windFactor + " °F";
 }
