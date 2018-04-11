e6d939e0ea6b63e9bc504284c0377fe2de751a09
Silva-Amon <amon.bernardo@hotmail.com>
2018-04-11 14:14:30 -0300

    sales page done

--- cit230/TheMountainSpoke/js/getSale.js
+++ cit230/TheMountainSpoke/js/getSale.js
@@ -5,7 +5,7 @@ $(function(){
         //getting items of products
         var imgAdd = "img/socialMedia/whatsapp.svg";
         $.each(prodJSON.products, function(i, prod){
-            $("#saleGrid").append("<section><h2>" + prod.name + "</h2> <p>Sizes: " + prod.sizes + "</p> <p>Price: $" + prod.price + "</p><p>Colors: " + prod.colors + "</p><p>Description: " + prod.description + "</p><img src='img/slideImg/"+ i +".jpg'></section>");
+            $("#saleGrid").append("<section><h2>" + prod.name + "</h2> <p>Sizes: " + prod.sizes + "</p> <p>Price: $" + prod.price + "</p><p>Colors: " + prod.colors + "</p><p>Description: " + prod.description + "</p></section><img src='img/sale/sale"+ i +".jpg'>");
         });
 
     });
