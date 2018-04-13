67674b2ebb0a1a079f395662d4194d49bc18bda1
Silva-Amon <amon.bernardo@hotmail.com>
2018-04-13 12:05:41 -0300

    slide show photos done =D

--- cit230/TheMountainSpoke/js/imgSlide.js
+++ cit230/TheMountainSpoke/js/imgSlide.js
@@ -1,3 +1,49 @@
 $(function(){
-    
-})
+    $("#one").css("display", "block");
+    var i = 1;
+    setInterval(function(){
+        if (i == 0){
+            $(".slideImg").fadeIn(400).css("display", "none");
+            $("#one").css("display", "block");
+            i++;
+            return;
+        }
+        if (i == 1){
+            $(".slideImg").fadeIn(400).css("display", "none");
+            $("#two").css("display", "block");
+            i++;
+            return;
+        }
+        if (i == 2){
+            $(".slideImg").fadeIn(400).css("display", "none");
+            $("#three").css("display", "block");
+            i = 0;
+            return;
+        }
+        
+    }, 2000);
+});
+
+/* -------------- Slide Show with SlideDown ------------------
+    setInterval(function(){
+        if (i == 0){
+            $(".slideImg").css("display", "none");
+            $("#one").slideDown().css("display", "block");
+            i++;
+            return;
+        }
+        if (i == 1){
+            $(".slideImg").css("display", "none");
+            $("#two").slideDown().css("display", "block");
+            i++;
+            return;
+        }
+        if (i == 2){
+            $(".slideImg").css("display", "none");
+            $("#three").slideDown().css("display", "block");
+            i = 0;
+            return;
+        }
+        
+    }, 2000);
+*/
