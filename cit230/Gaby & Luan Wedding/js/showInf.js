af259aeaa9badee9da7a48ae77394af2f39344b8
Silva-Amon <amon.bernardo@hotmail.com>
2018-08-08 13:12:30 -0300

    dar coisas scroll adapted
    ..

--- cit230/Gaby & Luan Wedding/js/showInf.js
+++ cit230/Gaby & Luan Wedding/js/showInf.js
@@ -1,5 +1,12 @@
 $(function(){
-   $("#showInf").click(function(){
-       $(".inf").slideToggle(500);
-   }); 
+    $("#showInf").click(function(){
+        $(".inf").slideToggle(500);
+        setTimeout(
+            function() 
+            {
+                $('html, body').stop().animate({
+                    scrollTop: $(".inf").offset().top
+                }, 600);
+            }, 300);
+    });
 });
