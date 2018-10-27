b9b015183dbabcdd80d0bdf37f2e1121812c33e3
Silva-Amon <amon.bernardo@hotmail.com>
2018-10-27 18:50:26 -0300

    Gaby site moved, and doWhile cit 261 fixed.

---
+++ SitedaHeigge/templet/templat.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Heigge S2</title>
+        <meta charset="utf-8">
+        <meta name="description" content="Venda de Herbalife, produtos, pedido para fazer sobrancelha, depilação e dicas de beleza em geral.">
+        <meta name="author" content="Amon T. B. da Silva - Heigge Aline Pinto Braga">
+        <meta name="keywords" content="Herbalife, Produto, Produtos, Nutrição, Nutricao, Nutricão, Nutrition, Vida Saudável, Saudavel, Saldavel, Saldaveu">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+</html>
