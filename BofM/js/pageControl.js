7a45a8d325d21f6c944bd5ec2510c78dbd337f80
Silva-Amon <amon.bernardo@hotmail.com>
2018-11-02 22:07:10 -0300

    pagecontrol.js created

--- BofM/js/pageControl.js
+++ BofM/js/pageControl.js
@@ -1,12 +1,14 @@
-document.getElementById("iFramPor").addEventListener("load", function(){
-    var url = this.contentWindow.location.href;
-    console.log(url);
-});
-
-
 function updatePage(){
-    var porURL = document.getElementById("iFramPor").getElementsByTagName("a");
-    var engURL = porURL.replace("por", "eng");
-    document.getElementById("iFramEng").src = engURL;
-    console.log(engURL);
+    var iFramPor;
+    document.getElementById("iFramPor").addEventListener("load", function(){
+        iFramPor = document.getElementById("iFramPor");
+    });    
+    console.log(iFramPor);
+    //    iFramPor = iFramPor.replace("#document","");
+
+    //    var porURL = document.getElementById("iFramPor").contentWindow.document.getElementsByTagName("a").href;
+    //    console.log(porURL);
+    //    var engURL = porURL.replace("por", "eng");
+    //    document.getElementById("iFramEng").src = engURL;
+    //    console.log(engURL);
 }
