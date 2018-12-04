4b48c61f7d462fbbb9b5250e36f0c4361d3ac67e
Silva-Amon <35241797+Silva-Amon@users.noreply.github.com>
2018-12-04 18:53:57 -0200

    last version mobile (mobile test yet)

--- cit261/FinalProject/js/sideMenuToggle.js
+++ cit261/FinalProject/js/sideMenuToggle.js
@@ -7,10 +7,18 @@ function closeSideMenu(){
     sideBar.style.transform = 'translateX(-1024px)';
 }
 function backMenu(){
-    var sideBar = document.getElementById('searchResults');
-    sideBar.style.transform = 'translateX(1024px)';
+    var searchResults = document.getElementById('searchResults');
+    searchResults.style.transform = 'translateX(1024px)';
+    
+    var footer = document.querySelector('footer');
+    footer.style.transform = 'translateX(1024px)'; 
+    
+    //restoring url
     var winUrl = window.location.href;
     var prodPos = winUrl.search("age_product");
     window.location.href = winUrl.substr(0, prodPos);
-    
+}
+function showAbout(){
+    var footer = document.querySelector('footer');
+    footer.style.transform = 'translateX(0px)'; 
 }
