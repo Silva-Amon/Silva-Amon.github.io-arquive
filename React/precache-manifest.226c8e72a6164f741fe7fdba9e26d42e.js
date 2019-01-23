4e6472969eeedff76a7f11394cc9e83393d97f44
Amon Silva <amon.bernardo@hotmail.com>
2019-01-23 21:07:25 -0200

    react

---
+++ React/precache-manifest.226c8e72a6164f741fe7fdba9e26d42e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bdeb66dadab5814ea446",
+    "url": "/React/static/js/runtime~main.bdeb66da.js"
+  },
+  {
+    "revision": "5dde5c55e8bbcbca055a",
+    "url": "/React/static/js/main.5dde5c55.chunk.js"
+  },
+  {
+    "revision": "f4789f78bc5099d1ff6c",
+    "url": "/React/static/js/1.f4789f78.chunk.js"
+  },
+  {
+    "revision": "5dde5c55e8bbcbca055a",
+    "url": "/React/static/css/main.21cfb6e0.chunk.css"
+  },
+  {
+    "revision": "4050328d7ff4ce03cca3e6e5e66f0e09",
+    "url": "/React/index.html"
+  }
+];
