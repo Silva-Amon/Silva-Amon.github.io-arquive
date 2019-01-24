2f97241cc587fc1c7293301ad795ea7ef9a8eb66
Amon Silva <amon.bernardo@hotmail.com>
2019-01-24 16:15:42 -0200

    email form send changed and site finished

--- Gaby&LuanWedding/js/remainderDays.js
+++ Gaby&LuanWedding/js/remainderDays.js
@@ -1,8 +1,11 @@
 var marryDay = new Date(2019,11,30);
 var today = new Date();
+
 var one_day = 1000*60*60*24;
 var DivBetweenDates = 0;
+
 var diffAnos = diffAnos = today.getYear() - marryDay.getYear();
+
 
 DivBetweenDates = (today.getMonth()+today.getDate()+diffAnos)/(marryDay.getMonth() + marryDay.getDate());
 
@@ -11,6 +14,8 @@ var percentage = Math.floor(DivBetweenDates * 100);
 var diffInDay = Math.abs(marryDay - today);
 
 var days = Math.floor(diffInDay/one_day)-29; //less one month, because Jan is 0, for some misterious reason, is -29 days than 30 (maybe because the leap year);
+
+
 
 document.getElementById("day").textContent = "♥ " + days + " Dias para o casamento ♥ " + percentage + "%";
 
