e41001934622d57e9b12a925f76ae8e1f67a8a07
Amon Silva <amon.bernardo@hotmail.com>
2020-05-02 22:21:28 -0300

    test build

---
+++ blogger/workout-timer/build/precache-manifest.09c4a404f92ae637fcaabe6b2df2c470.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8f2ff9f9e9cf916bffa5ef984779c5c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6daf1febd6276fa8dc23",
+    "url": "./static/css/main.92b2a25f.chunk.css"
+  },
+  {
+    "revision": "3c76e21c1ea23a4d8cc4",
+    "url": "./static/js/2.dc5adb23.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.dc5adb23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6daf1febd6276fa8dc23",
+    "url": "./static/js/main.7ad31bce.chunk.js"
+  },
+  {
+    "revision": "2dcce060666ad182fd61",
+    "url": "./static/js/runtime-main.ea2d7c12.js"
+  }
+]);
