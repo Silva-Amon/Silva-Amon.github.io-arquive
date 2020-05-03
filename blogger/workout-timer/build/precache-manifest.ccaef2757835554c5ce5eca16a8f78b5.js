266c7b521fc7c6c198d4072a6f6275997b0e8126
Amon Silva <amon.bernardo@hotmail.com>
2020-05-02 22:24:27 -0300

    package updated

---
+++ blogger/workout-timer/build/precache-manifest.ccaef2757835554c5ce5eca16a8f78b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cf8114fdf3751b5d218e3eadac571ea",
+    "url": "/blogger/workout-timer/build/index.html"
+  },
+  {
+    "revision": "6daf1febd6276fa8dc23",
+    "url": "/blogger/workout-timer/build/static/css/main.92b2a25f.chunk.css"
+  },
+  {
+    "revision": "3c76e21c1ea23a4d8cc4",
+    "url": "/blogger/workout-timer/build/static/js/2.dc5adb23.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/blogger/workout-timer/build/static/js/2.dc5adb23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6daf1febd6276fa8dc23",
+    "url": "/blogger/workout-timer/build/static/js/main.7ad31bce.chunk.js"
+  },
+  {
+    "revision": "f224a7f96b59eb56877f",
+    "url": "/blogger/workout-timer/build/static/js/runtime-main.237310c2.js"
+  }
+]);
