30593cfed6f69d0e03a1785d40952f99fcc22e01
Amon Silva <amon.bernardo@hotmail.com>
2020-05-02 22:05:35 -0300

    Updates

---
+++ precache-manifest.9c181c532cb990503643b5605604faa4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92a0f5aa07a0e8439e00a893ef743ca6",
+    "url": "/Silva-Amon/Silva-Amon.github.io/tree/master/blogger/workout-timer/index.html"
+  },
+  {
+    "revision": "6daf1febd6276fa8dc23",
+    "url": "/Silva-Amon/Silva-Amon.github.io/tree/master/blogger/workout-timer/static/css/main.92b2a25f.chunk.css"
+  },
+  {
+    "revision": "3c76e21c1ea23a4d8cc4",
+    "url": "/Silva-Amon/Silva-Amon.github.io/tree/master/blogger/workout-timer/static/js/2.dc5adb23.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Silva-Amon/Silva-Amon.github.io/tree/master/blogger/workout-timer/static/js/2.dc5adb23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6daf1febd6276fa8dc23",
+    "url": "/Silva-Amon/Silva-Amon.github.io/tree/master/blogger/workout-timer/static/js/main.7ad31bce.chunk.js"
+  },
+  {
+    "revision": "a3693c863c3487dcaf60",
+    "url": "/Silva-Amon/Silva-Amon.github.io/tree/master/blogger/workout-timer/static/js/runtime-main.76e132b3.js"
+  }
+]);
